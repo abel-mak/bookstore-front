3674446c12b627ea42b18fbe789de483f67c063a
abel-mak <abdelmkey19973@gmail.com>
2023-05-15 15:16:44 +0100

    add fetch from backend

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,30 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, HttpClientModule],
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+
+export class BookListComponent implements OnInit {
   books: any[] = [
     {
-      title: 'Book 1',
-      author: 'author name of Book 1'
+      name: 'Book 1',
+      authorName: 'author name of Book 1'
     },
     {
-      title: 'Book 2',
-      author: 'author name of Book 2'
+      name: 'Book 2',
+      authorName: 'author name of Book 2'
     },
     {
-      title: 'Book 3',
-      author: 'author name of Book 3'
+      name: 'Book 3',
+      authorName: 'author name of Book 3'
     }
   ];
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get("https://localhost:44328/api/app/book")
+      .subscribe({
+        next: (value : any) => {
+          this.books = value.items;
+          console.log(value);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+
+  }
 
   orderBook(book: any) {
     // Handle the order button click event here
